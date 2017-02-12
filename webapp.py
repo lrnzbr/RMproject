eefd3478ee5bd0c3212bd855f8be0eb01a1a3f61
violette17-meet <violette17@meet.mit.edu>
2017-02-12 17:56:26 +0200

    trying to deploy still

--- webapp.py
+++ webapp.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory, session as login_session
 #import ctypes
 #from ctypes import *
-import sys
-import logging
-import os
+#import sys
+#import logging
+#import os
 #from PIL import Image
 from werkzeug.utils import secure_filename
 UPLOAD_FOLDER = 'uploads'
